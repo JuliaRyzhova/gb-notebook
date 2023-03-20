7652874a840db20df17c39f9294c869f9a340c0c
Julia Ryzhova <16-j.lazy@bk.ru>
2023-03-20 12:45:29 +0300

    Dont work Main

--- src/notebook/view/UserView.java
+++ src/notebook/view/UserView.java
@@ -4,7 +4,6 @@ import notebook.controller.UserController;
 import notebook.model.User;
 import notebook.util.Commands;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
